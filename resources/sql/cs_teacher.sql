0e873fe852fd322c439193e5448823ac730acb0d
XiaoLFeng <gm@x-lf.cn>
2025-01-18 15:46:49 +0800

    refactor(resources): 优化教师信息表结构
    - 移除教师性别字段的默认值
    - 调整字段定义的缩进，提高代码可读性

--- resources/sql/cs_teacher.sql
+++ resources/sql/cs_teacher.sql
@@ -1,3 +1,31 @@
+/*
+ * --------------------------------------------------------------------------------
+ * Copyright (c) 2022-NOW(至今) 锋楪技术团队
+ * Author: 锋楪技术团队 (https://www.frontleaves.com)
+ *
+ * 本文件包含锋楪技术团队项目的源代码，项目的所有源代码均遵循 MIT 开源许可证协议。
+ * --------------------------------------------------------------------------------
+ * 许可证声明：
+ *
+ * 版权所有 (c) 2022-2025 锋楪技术团队。保留所有权利。
+ *
+ * 本软件是“按原样”提供的，没有任何形式的明示或暗示的保证，包括但不限于
+ * 对适销性、特定用途的适用性和非侵权性的暗示保证。在任何情况下，
+ * 作者或版权持有人均不承担因软件或软件的使用或其他交易而产生的、
+ * 由此引起的或以任何方式与此软件有关的任何索赔、损害或其他责任。
+ *
+ * 使用本软件即表示您了解此声明并同意其条款。
+ *
+ * 有关 MIT 许可证的更多信息，请查看项目根目录下的 LICENSE 文件或访问：
+ * https://opensource.org/licenses/MIT
+ * --------------------------------------------------------------------------------
+ * 免责声明：
+ *
+ * 使用本软件的风险由用户自担。作者或版权持有人在法律允许的最大范围内，
+ * 对因使用本软件内容而导致的任何直接或间接的损失不承担任何责任。
+ * --------------------------------------------------------------------------------
+ */
+
 USE `class-scheduling-system`;
 CREATE TABLE `cs_teacher`
 (
@@ -8,7 +36,7 @@ CREATE TABLE `cs_teacher`
     `name`         VARCHAR(32)  NOT NULL COMMENT '教师姓名',
     `english_name` VARCHAR(128) NOT NULL COMMENT '教师英文名',
     `ethnic`       CHAR(32)     NOT NULL COMMENT '教师民族',
-    `sex`          BOOLEAN      NOT NULL DEFAULT TRUE COMMENT '教师性别 0:女 1:男',
+    `sex`          BOOLEAN      NOT NULL COMMENT '教师性别 0:女 1:男',
     `phone`        VARCHAR(16)  NULL COMMENT '教师电话',
     `email`        VARCHAR(255) NULL COMMENT '教师邮箱',
     `job_title`    VARCHAR(32)  NULL COMMENT '教师职称',
@@ -24,4 +52,4 @@ ALTER TABLE `cs_teacher`
     ADD CONSTRAINT `fk_teacher_unit_uuid`
         FOREIGN KEY (`unit_uuid`) REFERENCES `cs_department` (`department_uuid`),
     ADD CONSTRAINT `fk_teacher_user_uuid`
-        FOREIGN KEY (`user_uuid`) REFERENCES `cs_user` (`user_uuid`);
+    FOREIGN KEY (`user_uuid`) REFERENCES `cs_user` (`user_uuid`);
